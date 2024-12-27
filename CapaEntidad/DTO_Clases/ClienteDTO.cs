b6c84f387cd9dd4261618714ef2a29919d1e30ca
Kuma <mauro_11eze@hotmail.com>
2024-12-27 20:03:39 -0300

    Se agrego la lista de ventas

--- CapaEntidad/DTO_Clases/ClienteDTO.cs
+++ CapaEntidad/DTO_Clases/ClienteDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CapaDatos.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,5 +16,6 @@ namespace Application_Layer.DTO_Clases
         public string? Telefono { get; set; }
 
         public string? Correo { get; set; }
+        public ICollection<Venta> ListaVenta { get; set; } = new List<Venta>();
     }
 }
